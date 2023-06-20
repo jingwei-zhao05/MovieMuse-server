307cc939c642f9a28c72ca94bc8fa5d3f78106be
Jingwei Zhao <jingwei.zhao0527@gmail.com>
2023-06-19 23:51:03 -0400

    Finish server side authentication & authorization

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,6 @@
 const knex = require("knex")(require("../knexfile"));
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const getUsers = (_req, res) => {
   knex("users")
@@ -28,7 +30,54 @@ const createUser = (req, res) => {
     });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Login requires email and password",
+    });
+  }
+
+  // if match => send the token (jwt.sign ({ id }))
+  knex("users")
+    .where({ email: email })
+    .then((users) => {
+      if (users.length === 0) {
+        return res.status(401).json({
+          message: "Invalid credentials",
+        });
+      }
+
+      const user = users[0];
+
+      // const validPassword = bcrypt.compareSync(password, user.password);
+      const validPassword = password === user.password;
+      if (!validPassword) {
+        return res.status(401).json({
+          message: "Invalid credentials",
+        });
+      }
+
+      const token = jwt.sign(
+        { userName: user.user_name },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: 60 * 60 * 24,
+        }
+      );
+
+      res.json({ token });
+    });
+};
+
+const selectMovies = (req, res) => {
+  res.json(req.userName);
+};
+
 module.exports = {
   getUsers,
   createUser,
+  login,
+  selectMovies,
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const userController = require("../controllers/user-controller");
 const { validateUser } = require("../middleware/user-validator");
+const { authorize } = require("../middleware/authorize");
+const jwt = require("jsonwebtoken");
 
 router
   .route("/")
   .get(userController.getUsers)
   .post(validateUser, userController.createUser);
+
+router.route("/login").post(userController.login);
+
+router.route("/select-movies").get(authorize, userController.selectMovies);
 
 module.exports = router;
